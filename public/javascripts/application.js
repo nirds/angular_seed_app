54532ecc94583ed97d28f069db2504cdcea032dd
Christopher Spring <chris.spring@gmail.com>
2013-08-11 13:39:30 +0200

    changing url, controllers and templates

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,16 +1,48 @@
-function MyController($scope){
-  $scope.items = [
-    {text: 'Initialized in the controller'},
-    {text: 'Also Initialized in the controller'}
+var app = angular.module('MyApp', []);
+
+app.config(function($routeProvider, $locationProvider){
+  $locationProvider.html5Mode(true);
+  $routeProvider.
+    when("/index.html",    {templateUrl: '/templates/index.html', controller: 'IndexController'}).
+    when("/show/:id.html", {templateUrl: '/templates/show.html',  controller: 'ShowController'}).
+    otherwise({redirectTo: '/index.html'})
+});
+
+app.factory("Person", function() {
+  var people = [
+    { firstName: "Brad",     lastName: 'Pitt',   age: 78,  eyes: 'Green', id: 1 },
+    { firstName: "Angilina", lastName: 'Jolie',  age: 104, eyes: 'Blue',  id: 2 },
+    { firstName: "Tom",      lastName: 'Cruise', age: 981, eyes: 'Two',   id: 3 }
   ];
 
-  $scope.addItem = function(){
-    $scope.items.push({text: $scope.itemText});
-    $scope.itemText = '';
-  };
+  return {
+    all: function() {
+      return people;
+    },
 
-  $scope.clearItems = function(){
-    $scope.itemText = '';
-    $scope.items = [];
+    find: function(id) {
+      var result = null;
+      angular.forEach(people, function(p) {
+        if (p.id == id){
+          result = p;
+        }
+      });
+      return result;
+    }
+  };
+});
+
+app.controller("IndexController", function($scope, $location, Person) {
+  $scope.people = Person.all();
+
+  $scope.show = function(person){
+    $location.path('/show/' + person.id + '.html')
   }
-}
+});
+
+app.controller("ShowController", function($scope, $routeParams, $window, Person) {
+  $scope.person = Person.find($routeParams.id);
+  $scope.back = function(){
+    $window.history.back();
+  };
+});
