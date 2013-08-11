8990b2c69ddae1ffcdb6a8ade04c741cfeafb84a
Christopher Spring <chris.spring@gmail.com>
2013-08-11 19:40:53 +0200

    consuming google finance api

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,17 @@
-function MyController($scope, $http){
-  $http.get('json/people.json').
-  success(function(data, status, headers, config){
-   $scope.people = data;
-  }).
-  error(function(data, status, headers, config){
-    console.error('Could not load people')
-  });
-};
+var app = angular.module("MyApp", ["ngResource"]);
+
+app.controller("MyController", function($scope, $resource){
+  $scope.searchTerm    = 'GOOG'
+
+  $scope.googleFinance = $resource(
+    'https://finance.google.com/finance/info',
+    {client:'ig', callback:'JSON_CALLBACK'},
+    {get: {method:'JSONP', isArray: true}}
+  );
+
+  $scope.search = function() {
+    $scope.searchResults = $scope.googleFinance.get(
+      { q: $scope.searchTerm }
+    );
+  };
+});
